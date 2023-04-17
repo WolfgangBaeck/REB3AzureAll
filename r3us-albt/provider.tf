739c2775d344c09070897286746a352b946ad7b2
Wolfgang Baeck <wbaeck@earthlink.net>
2023-04-17 15:45:16 -0600

    initial commit

---
+++ r3us-albt/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+    backend "azurerm" {
+    resource_group_name  = "terraform-tfstate-rg"
+    storage_account_name = "terraformtfv78lno1s"
+    container_name       = "albt-tfstate"
+    key                  = "actions.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
